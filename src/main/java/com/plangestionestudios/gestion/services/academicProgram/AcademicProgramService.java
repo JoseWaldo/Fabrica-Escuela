eef615b9829aea9049dbec06aad1792befb2550c
Valentina Cadena <valentina.cadena@udea.edu.co>
2023-05-21 16:39:37 -0500

    feat: añadir interface AcademicProgram

---
+++ src/main/java/com/plangestionestudios/gestion/services/academicProgram/AcademicProgramService.java
@@ -0,0 +1,13 @@
+package com.plangestionestudios.gestion.services.academicProgram;
+
+import java.util.List;
+
+public interface AcademicProgramService {
+    public boolean deleteAcademicProgram(int id);
+    /*
+    public List<AcademicUnit> getAllAcademicUnits();
+    public AcademicUnit updateAcademicUnit(int id, AcademicUnit academicUnit);
+    public AcademicUnit getAcademicUnitById(int id);
+    public AcademicUnit createAcademicUnit(AcademicUnit academicUnit);
+     */
+}
