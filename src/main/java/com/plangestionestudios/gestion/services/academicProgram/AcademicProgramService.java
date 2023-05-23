7765f0f77aeba97c99eab5108e44a936fe2af439
Valentina Cadena <valentina.cadena@udea.edu.co>
2023-05-23 17:11:02 -0500

    feat: Crear método de obtener programa por id

--- src/main/java/com/plangestionestudios/gestion/services/academicProgram/AcademicProgramService.java
+++ src/main/java/com/plangestionestudios/gestion/services/academicProgram/AcademicProgramService.java
@@ -1,13 +1,16 @@
 package com.plangestionestudios.gestion.services.academicProgram;
 
+import com.plangestionestudios.gestion.entities.AcademicProgram;
+
 import java.util.List;
 
 public interface AcademicProgramService {
     public boolean deleteAcademicProgram(int id);
+    public AcademicProgram getAcademicProgramById(int id);
+
     /*
     public List<AcademicUnit> getAllAcademicUnits();
     public AcademicUnit updateAcademicUnit(int id, AcademicUnit academicUnit);
-    public AcademicUnit getAcademicUnitById(int id);
     public AcademicUnit createAcademicUnit(AcademicUnit academicUnit);
      */
 }
