42646d227b385eb590776828c60d0f80977a2ad5
Valen MR <valentinamunoz1010@gmail.com>
2023-05-25 11:37:50 -0500

    feat: creacion interfaz de los servicios de la linea de enfasis

--- src/main/java/com/plangestionestudios/gestion/services/emphasisLine/EmphasisLineService.java
+++ src/main/java/com/plangestionestudios/gestion/services/emphasisLine/EmphasisLineService.java
@@ -1,2 +1,16 @@
-package com.plangestionestudios.gestion.services.emphasisLine;public interface EmphasisLineService {
+package com.plangestionestudios.gestion.services.emphasisLine;
+
+import com.plangestionestudios.gestion.entities.AcademicUnit;
+import com.plangestionestudios.gestion.entities.EmphasisLine;
+
+import java.util.List;
+
+public interface EmphasisLineService {
+
+    public boolean deleteEmphasisLine(int id);
+    public List<EmphasisLine> getAllEmphasisLine();
+    public EmphasisLine updateEmphasisLine(int id, EmphasisLine emphasisLine);
+    public EmphasisLine getEmphasisLineById(int id);
+    public EmphasisLine createEmphasisLine(EmphasisLine emphasisLine);
+
 }
