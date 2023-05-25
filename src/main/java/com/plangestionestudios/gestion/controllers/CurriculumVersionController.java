fc20bad6c80424a65dc0449d9084e23bacdc9aca
Jose Waldo <josferw@gmail.com>
2023-05-25 15:23:06 -0500

    feat: añadi el controlador para crear una version del p.d.e

--- src/main/java/com/plangestionestudios/gestion/controllers/CurriculumVersionController.java
+++ src/main/java/com/plangestionestudios/gestion/controllers/CurriculumVersionController.java
@@ -1,2 +1,27 @@
-package com.plangestionestudios.gestion.controllers;public class CurriculumVersionController {
+package com.plangestionestudios.gestion.controllers;
+
+import com.plangestionestudios.gestion.entities.CurriculumVersion;
+import com.plangestionestudios.gestion.services.curriculumVersion.CurriculumVersionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/curriculumVersion")
+@RestController
+public class CurriculumVersionController {
+
+    @Autowired
+    private CurriculumVersionService curriculumVersionService;
+
+    @PostMapping("")
+    public ResponseEntity<CurriculumVersion> createCurriculumVersion(@RequestBody CurriculumVersion curriculumVersion){
+        CurriculumVersion curriculumVersionCreated = this.curriculumVersionService.createCurriculumVersion(curriculumVersion);
+        if(curriculumVersionCreated != null) return new ResponseEntity<>(curriculumVersionCreated, HttpStatus.CREATED);
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+
 }
