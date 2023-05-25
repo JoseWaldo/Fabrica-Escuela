2bb925adca6755c510040a74e270b4a2cf4cdae3
Jose Waldo <josferw@gmail.com>
2023-05-25 14:34:50 -0500

    feat: creacion del archivo de implementacion de servicios de las versiones del p.d.e

--- src/main/java/com/plangestionestudios/gestion/services/curriculumVersion/CurriculumVersionServiceImp.java
+++ src/main/java/com/plangestionestudios/gestion/services/curriculumVersion/CurriculumVersionServiceImp.java
@@ -1,2 +1,32 @@
-package com.plangestionestudios.gestion.services.curriculumVersion;public class CurriculumVersionServiceImp {
+package com.plangestionestudios.gestion.services.curriculumVersion;
+
+import com.plangestionestudios.gestion.entities.CurriculumVersion;
+
+import java.util.List;
+
+public class CurriculumVersionServiceImp implements CurriculumVersionService {
+    @Override
+    public boolean deleteCurriculumVersion(int id) {
+        return false;
+    }
+
+    @Override
+    public List<CurriculumVersion> getAllCurriculumVersion() {
+        return null;
+    }
+
+    @Override
+    public CurriculumVersion getByIdCurriculumVersion(int id) {
+        return null;
+    }
+
+    @Override
+    public CurriculumVersion updateCurriculumVersion(int id, CurriculumVersion curriculumVersion) {
+        return null;
+    }
+
+    @Override
+    public CurriculumVersion createCurriculumVersion(CurriculumVersion curriculumVersion) {
+        return null;
+    }
 }
