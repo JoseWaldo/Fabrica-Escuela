99f14b1c0a4306074703035af84c8114b86e94a6
Jose Waldo <josferw@gmail.com>
2023-05-25 15:21:38 -0500

    feat: añadi una funcionalidad del repositorio de version para poder buscar por el numero de la version

--- src/main/java/com/plangestionestudios/gestion/repositories/CurriculumVersionRepository.java
+++ src/main/java/com/plangestionestudios/gestion/repositories/CurriculumVersionRepository.java
@@ -3,5 +3,8 @@ package com.plangestionestudios.gestion.repositories;
 import com.plangestionestudios.gestion.entities.CurriculumVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CurriculumVersionRepository extends JpaRepository<CurriculumVersion, Integer> {
+    List<CurriculumVersion> findByNumCurriculumVersion(int numCurriculumVersion);
 }
