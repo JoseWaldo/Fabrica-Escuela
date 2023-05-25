978fb7f64a09e91cf8fe1955ac40edea99da3f49
Valen MR <valentinamunoz1010@gmail.com>
2023-05-25 11:37:00 -0500

    feat: creacion repositorio de linea de enfasis

--- src/main/java/com/plangestionestudios/gestion/repositories/EmphasisLineRepository.java
+++ src/main/java/com/plangestionestudios/gestion/repositories/EmphasisLineRepository.java
@@ -1,2 +1,12 @@
-package com.plangestionestudios.gestion.repositories;public interface EmphasisLineRepository {
+package com.plangestionestudios.gestion.repositories;
+
+import com.plangestionestudios.gestion.entities.AcademicUnit;
+import com.plangestionestudios.gestion.entities.EmphasisLine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+public interface EmphasisLineRepository extends JpaRepository<EmphasisLine, Integer> {
+
+    List<EmphasisLine> findByIdEmphasisLine(String idEmphasisLine);
+
 }
