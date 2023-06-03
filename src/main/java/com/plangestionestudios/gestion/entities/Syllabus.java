d56164580d29920b910aefbf7febaa632b5002c5
Camilo avendaño <camilo.avendano1@udea.edu.co>
2023-06-03 15:17:36 -0500

    added: entidad syllabus y sus atributos de conexion

--- src/main/java/com/plangestionestudios/gestion/entities/Syllabus.java
+++ src/main/java/com/plangestionestudios/gestion/entities/Syllabus.java
@@ -1,4 +1,24 @@
 package com.plangestionestudios.gestion.entities;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "plan_de_estudios")
 public class Syllabus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private int idSyllabus;
+    @Column(name = "nombre_plan_estudios",unique = true, nullable = false)
+    private String nameCourse;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_academic_program", nullable = false)
+    @JsonBackReference
+    private AcademicUnit academicUnit;
 }
