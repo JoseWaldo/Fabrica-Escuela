710749661996a5f788af8841c2f707060fce0695
Jose Waldo <josferw@gmail.com>
2023-06-03 09:13:02 -0500

    Test: correccion en algunos test

--- src/test/java/com/plangestionestudios/gestion/AcademicSubUnitControllerTest.java
+++ src/test/java/com/plangestionestudios/gestion/AcademicSubUnitControllerTest.java
@@ -1,5 +1,7 @@
 package com.plangestionestudios.gestion;
 
+import com.plangestionestudios.gestion.controllers.AcademicUnitController;
+import com.plangestionestudios.gestion.entities.AcademicUnit;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,24 +17,51 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AcademicSubUnitControllerTest {
     @Autowired
     private AcademicSubUnitController academicSubUnitController;
+    @Autowired
+    private AcademicUnitController academicUnitController;
     static AcademicSubUnit subAcademicUnit = new AcademicSubUnit();
-
+    static AcademicUnit academicUnit = new AcademicUnit();
     @BeforeAll
     public static void setUp() {
+        academicUnit.setNameAcademicUnit("name");
+        academicUnit.setCodeAcademicUnit("code");
+        academicUnit.setTypeAcademicUnit("facultad");
+        academicUnit.setDeanName("dean");
+        academicUnit.setDescription("description");
+        academicUnit.setUbicationAcademicUnit("ubication");
+        academicUnit.setCostCenterCode("costCenter");
+
         subAcademicUnit.setCodeAcademicSubUnit("code");
         subAcademicUnit.setNameAcademicSubUnit("name");
-        subAcademicUnit.setTypeAcademicSubUnit("type");
+        subAcademicUnit.setTypeAcademicSubUnit("departamento");
         subAcademicUnit.setHeadName("head");
         subAcademicUnit.setDescription("description");
     }
 
     @Test
-    void testGetAllAcademicUnits() {
+    void testGetAllAcademicSubUnits() {
         assertEquals(HttpStatus.OK, academicSubUnitController.getAllAcademicSubUnits().getStatusCode());
     }
 
     @Test
-    void testCreateAcademicUnits() {
+    void testGetAcademicSubUnitById() {
+        ResponseEntity<AcademicSubUnit> response = academicSubUnitController.getAcademicSubUnitById(-1);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
+        academicUnitController.createAcademicUnits(academicUnit);
+        subAcademicUnit.setAcademicUnit(academicUnit);
+
+        academicSubUnitController.createAcademicSubUnits(subAcademicUnit);
+
+        response = academicSubUnitController.getAcademicSubUnitById(subAcademicUnit.getIdAcademicSubUnit());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        academicUnitController.deleteBloque(academicUnit.getIdAcademicUnit());
+        academicSubUnitController.deleteAcademicSubUnit(subAcademicUnit.getIdAcademicSubUnit());
+    }
+
+    @Test
+    void testCreateAcademicSubUnits() {
         ResponseEntity<AcademicSubUnit> response = academicSubUnitController.createAcademicSubUnits(subAcademicUnit);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
