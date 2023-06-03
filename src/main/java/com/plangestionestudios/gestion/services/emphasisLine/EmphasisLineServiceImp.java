fd2fc7732f0221e816a26438b1f1e456671157c8
Valen MR <valentinamunoz1010@gmail.com>
2023-06-03 14:03:50 -0500

    Arreglando la clase de servicios

--- src/main/java/com/plangestionestudios/gestion/services/emphasisLine/EmphasisLineServiceImp.java
+++ src/main/java/com/plangestionestudios/gestion/services/emphasisLine/EmphasisLineServiceImp.java
@@ -1,2 +1,49 @@
-package com.plangestionestudios.gestion.services.emphasisLine;public class EmphasisLineServiceImp {
+package com.plangestionestudios.gestion.services.emphasisLine;
+
+import com.plangestionestudios.gestion.entities.AcademicUnit;
+import com.plangestionestudios.gestion.entities.EmphasisLine;
+import com.plangestionestudios.gestion.repositories.AcademicUnitRepository;
+import com.plangestionestudios.gestion.repositories.EmphasisLineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+public class EmphasisLineServiceImp implements EmphasisLineService {
+
+
+    private EmphasisLineRepository emphasisLineRepository;
+    @Override
+    public boolean deleteEmphasisLine(int id) {
+        EmphasisLine emphasisLineFound = this.getEmphasisLineById(id);
+        if(emphasisLineFound != null){
+            emphasisLineRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<EmphasisLine> getAllEmphasisLine() {
+        List<EmphasisLine> emphasisLineList = emphasisLineRepository.findAll();
+        return emphasisLineList;
+    }
+
+    @Override
+    public EmphasisLine updateEmphasisLine(int id, EmphasisLine emphasisLine) {
+        return null;
+    }
+
+    @Override
+    public EmphasisLine getEmphasisLineById(int id) {
+        Optional<EmphasisLine> emphasisLine = emphasisLineRepository.findById(id);
+        if(emphasisLine.isPresent()) return emphasisLine.get();
+        return null;
+
+    }
+
+    @Override
+    public EmphasisLine createEmphasisLine(EmphasisLine emphasisLine) {
+        return null;
+    }
 }
