45137437c80032786df87c8a3bec5f728152917b
Camilo avendaño <camilo.avendano1@udea.edu.co>
2023-06-03 15:23:09 -0500

    added: services methods in interface for Syllabus

--- src/main/java/com/plangestionestudios/gestion/services/syllabus/SyllabusImp.java
+++ src/main/java/com/plangestionestudios/gestion/services/syllabus/SyllabusImp.java
@@ -1,4 +1,32 @@
 package com.plangestionestudios.gestion.services.syllabus;
 
+import com.plangestionestudios.gestion.entities.AcademicSubUnit;
+
+import java.util.List;
+
 public class SyllabusImp implements SyllabusServices{
+    @Override
+    public boolean deleteSyllabus(int id) {
+        return false;
+    }
+
+    @Override
+    public List<AcademicSubUnit> getAllSyllabus() {
+        return null;
+    }
+
+    @Override
+    public AcademicSubUnit getSyllabusById(int id) {
+        return null;
+    }
+
+    @Override
+    public AcademicSubUnit updateSyllabus(int id, AcademicSubUnit academicSubUnit) {
+        return null;
+    }
+
+    @Override
+    public AcademicSubUnit createSyllabus(AcademicSubUnit academicSubUnit) {
+        return null;
+    }
 }
--- src/main/java/com/plangestionestudios/gestion/services/syllabus/SyllabusServices.java
+++ src/main/java/com/plangestionestudios/gestion/services/syllabus/SyllabusServices.java
@@ -1,4 +1,13 @@
 package com.plangestionestudios.gestion.services.syllabus;
 
+import com.plangestionestudios.gestion.entities.AcademicSubUnit;
+
+import java.util.List;
+
 public interface SyllabusServices {
+    public boolean deleteSyllabus(int id);
+    public List<AcademicSubUnit> getAllSyllabus();
+    public AcademicSubUnit getSyllabusById(int id);
+    public AcademicSubUnit updateSyllabus(int id, AcademicSubUnit academicSubUnit);
+    public AcademicSubUnit createSyllabus(AcademicSubUnit academicSubUnit);
 }
