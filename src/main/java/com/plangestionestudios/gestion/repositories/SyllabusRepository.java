07209670d141d4dd8d91ee844ad75ac20919513f
Camilo avendaño <camilo.avendano1@udea.edu.co>
2023-06-03 15:17:54 -0500

    added: JpaRepository and seach by id

--- src/main/java/com/plangestionestudios/gestion/repositories/SyllabusRepository.java
+++ src/main/java/com/plangestionestudios/gestion/repositories/SyllabusRepository.java
@@ -1,4 +1,11 @@
 package com.plangestionestudios.gestion.repositories;
 
-public interface SyllabusRepository {
+import com.plangestionestudios.gestion.entities.EmphasisLine;
+import com.plangestionestudios.gestion.entities.Syllabus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SyllabusRepository extends JpaRepository<Syllabus, Integer> {
+    List<Syllabus> findByIdSyllabus(String idSyllabus);
 }
